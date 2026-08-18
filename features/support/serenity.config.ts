40bf15742f7709fc5fb57b57528dade0036475f3
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-08-18 16:31:31 +0100

    chore: migrate from serenity-bdd to html-reporter
      - Replace @serenity-js/serenity-bdd with @serenity-js/html-reporter
      - Remove npm-failsafe, rimraf, http-server, mkdirp
      - Update @serenity-js/* modules to 3.45.7
      - Simplify scripts: test runs cucumber-js directly, test:report serves the report
      - Update features/support/serenity.config.ts crew config
      - Update GitHub Actions workflow with trend history preservation
      - Fix devcontainer: remove desktop-lite (API-only), fix postAttachCommand race condition
      - Update README to reflect new setup (no Java required)

--- features/support/serenity.config.ts
+++ features/support/serenity.config.ts
@@ -11,13 +11,15 @@ import { Actors } from '../../src';
  * @see https://serenity-js.org/handbook/reporting/index.html
  */
 BeforeAll(function () {
-    // Configure Serenity/JS
+    // Configure Serenity/JS reporting services
     configure({
         // Configure Serenity/JS reporting services
         crew: [
             [ '@serenity-js/console-reporter', { theme: 'auto' } ],
-            [ '@serenity-js/core:ArtifactArchiver', { outputDirectory: path.resolve(__dirname, '../../target/site/serenity') } ],
-            [ '@serenity-js/serenity-bdd', { specDirectory: path.resolve(__dirname, '../../features') } ],
+            [ '@serenity-js/html-reporter', {
+                outputDirectory: path.resolve(__dirname, '../../reports/serenity-js'),
+                specDirectory: path.resolve(__dirname, '../../features'),
+            } ],
         ],
     });
 });
